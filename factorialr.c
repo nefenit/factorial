d1014489aa19b689501c915a4cc97f6ada57aa44
nefenit <nefenit@gmail.com>
2019-03-10 22:59:53 +0100

    Update factorialr.c

--- factorialr.c
+++ factorialr.c
@@ -6,11 +6,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void usage(int status) {
+	fprintf(stderr, "usage: factorialr N");
+	exit(status);
+}
+
 unsigned long long factorial_recursive(unsigned long long n) {
 	return n ? n * factorial_recursive(n-1) : 1;
 }
 
 int main(int argc, char **argv) {
+	if(argc != 2)
+		usage(EXIT_FAILURE);
 	printf("%llu\n", factorial_recursive(strtoull(argv[1], NULL, 10)));
-	return 0;
+	return EXIT_SUCCESS;
 }
